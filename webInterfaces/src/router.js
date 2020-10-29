61fcfbf7ea7afa3378dcdeac74595e430615ea03
Romain PIOT <romainpiot.pro@gmail.com>
2020-10-29 17:30:04 +0100

    todo content of function before each in router.js

--- webInterfaces/src/router.js
+++ webInterfaces/src/router.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 import WorkingTimes from './components/WorkingTimes.vue';
 import WorkingTime from './components/WorkingTime.vue';
 import ClockManager from './components/ClockManager.vue';
 import ChartManager from './components/ChartManager.vue';
 import login from './components/Auth/Login.vue';
+import store from './store/store';
 
 Vue.use(Router);
 const router = new Router({
@@ -40,8 +41,30 @@ const router = new Router({
       name: 'signIn',
       component: login,
     },
-    { path: '/', redirect: '/workingTimes/1'}
+    { path: '/', redirect: '/workingTimes/1' },
   ],
 });
 
-export default router
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const { authorize } = to.meta;
+  const currentUser = store.state.userId;
+  console.log(currentUser);
+
+  if (authorize) {
+      if (!currentUser) {
+          // not logged in so redirect to login page with the return url
+          return next({ path: '/login', query: { returnUrl: to.path } });
+      }
+
+      // check if route is restricted by role
+      if (authorize.length && !authorize.includes(currentUser.role)) {
+          // role not authorised so redirect to home page
+          return next({ path: '/' });
+      }
+  }
+
+  next();
+})
+
+export default router;
