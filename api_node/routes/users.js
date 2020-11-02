e91c9dc163a9f4ca40214f783e7f76d3f49c8dc2
Romain PIOT <romainpiot.pro@gmail.com>
2020-11-02 20:08:07 +0100

    add functions for associations user team

--- api_node/routes/users.js
+++ api_node/routes/users.js
@@ -5,6 +5,8 @@ const user = require('../controllers/userController');
 router.get('/', user.getAll);
 router.get('/:user_id', user.getById);
 router.put('/:user_id', user.update);
+router.get('/team/:user_team', user.getTeam);
+router.post('/:user_id/update-teams', user.updateTeams);
 router.delete('/:user_id', user.delete);
 
 module.exports = router;
