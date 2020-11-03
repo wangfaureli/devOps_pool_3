dea967b067f5654a35d31eca2151cce70dbd093a
Romain PIOT <romainpiot.pro@gmail.com>
2020-11-03 17:44:20 +0100

    fix route

--- api_node/routes/globals.js
+++ api_node/routes/globals.js
@@ -5,6 +5,6 @@ const user = require('../controllers/userController');
 router.post('/logout', user.logout);
 router.post('/register', user.create);
 router.post('/login', user.login);
-router.post('/checkToken', user.checkUserData);
+router.post('/check-token', user.checkUserData);
 
 module.exports = router;
