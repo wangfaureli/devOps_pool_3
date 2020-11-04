29a9d0a15cffd9b6edfdd9831a29ef089f57ad33
jatin-bhatia1 <bhatia.jatin1997@gmail.com>
2020-11-04 01:01:13 +0100

    auth changes

--- webInterfaces/src/store/store.js
+++ webInterfaces/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+//import axios from 'axios';
+import { apiUrl } from "../settings.js";
 // import { reject } from 'core-js/fn/promise';
 
 Vue.use(Vuex);
@@ -16,25 +17,12 @@ const store = new Vuex.Store({
   actions: {
     recoverToken(context, credentials) {
       console.log(credentials);
-      return new Promise((resolve, reject) => {
-        axios
-          .post('/users/sign_in', {
-            email: credentials.email,
-            password: credentials.password,
-          })
-          .then((response) => {
-            const token = response.data.access_token;
-            localStorage.setItem('access_token', token);
-            context.commit('recovertoken', token);
-            resolve(response);
-          })
-          .catch((error) => {
-            console.log(error);
-            reject(error);
-          });
+      let data = JSON.stringify({
+        "user":{
+          username: credentials.username,
+          password: credentials.password
+        }
       });
-<<<<<<< Updated upstream
-=======
 
       (async () => {
         const rawResponse = await fetch(`${apiUrl}/login`, {
@@ -67,8 +55,11 @@ const store = new Vuex.Store({
       //       reject(error);
       //     });
       // });
->>>>>>> Stashed changes
     },
+
+    beforeEach(){
+      
+    }
   },
 });
 
